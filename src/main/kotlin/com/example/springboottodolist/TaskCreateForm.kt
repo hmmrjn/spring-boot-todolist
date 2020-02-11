1d764dd3aec7e8bcf0593a671696e38813df108c
Jun Hamamura <hmmrjn@gmail.com>
2020-02-11 21:59:52 +0900

    Update TaskCreateForm to data class
    because it works

--- src/main/kotlin/com/example/springboottodolist/TaskCreateForm.kt
+++ src/main/kotlin/com/example/springboottodolist/TaskCreateForm.kt
@@ -3,8 +3,6 @@ package com.example.springboottodolist
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.Size
 
-class TaskCreateForm {
-    @NotBlank
-    @Size(max = 20)
-    var content: String? = null
-}
+data class TaskCreateForm (
+    @NotBlank @Size(max = 20) var content: String? = null
+)
