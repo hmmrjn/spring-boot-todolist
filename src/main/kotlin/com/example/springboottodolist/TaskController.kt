52915306ec69ec4cb74b58948829ab036b9b067e
Jun Hamamura <hmmrjn@gmail.com>
2020-02-11 16:02:44 +0900

    Create tasks/index.html

--- src/main/kotlin/com/example/springboottodolist/TaskController.kt
+++ src/main/kotlin/com/example/springboottodolist/TaskController.kt
@@ -1,18 +1,20 @@
 package com.example.springboottodolist
 
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
 
-@RestController
+@Controller
 @RequestMapping("tasks")
 class TaskController {
     @GetMapping("")
-    fun index() :String {
+    fun index(model: Model) :String {
         val tasks = listOf(
                 Task(1, "たまご", false),
-                Task(2, "牛乳", false)
+                Task(2, "牛乳", true)
         )
-        return tasks.toString()
+        model.addAttribute("tasks", tasks)
+        return "tasks/index"
     }
 }
