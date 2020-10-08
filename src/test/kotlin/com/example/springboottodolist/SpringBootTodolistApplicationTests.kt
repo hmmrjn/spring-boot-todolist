d3af7f3fe441c6feaa5259b9302d32dfe3faef7b
Jun Hamamura <hmmrjn@gmail.com>
2020-10-08 21:02:12 +0900

    Fix import

--- src/test/kotlin/com/example/springboottodolist/SpringBootTodolistApplicationTests.kt
+++ src/test/kotlin/com/example/springboottodolist/SpringBootTodolistApplicationTests.kt
@@ -1,6 +1,7 @@
 package com.example.springboottodolist
 
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
