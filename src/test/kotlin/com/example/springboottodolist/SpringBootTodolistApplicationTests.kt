e033103d93b07b296c0118e130a64c7b97cbfc46
hamajun <rodhamjun@hotmail.com>
2020-10-08 21:00:01 +0900

    Update SpringBootTodolistApplicationTests.kt

--- src/test/kotlin/com/example/springboottodolist/SpringBootTodolistApplicationTests.kt
+++ src/test/kotlin/com/example/springboottodolist/SpringBootTodolistApplicationTests.kt
@@ -8,7 +8,18 @@ import org.springframework.boot.test.context.SpringBootTest
 class SpringBootTodolistApplicationTests {
 
 	@Test
-	fun contextLoads() {
+	fun successExample() {
+		assertEquals(123, 123)
+	}
+	
+	@Test
+	fun failExample() {
+		assertEquals(123, 444)
+	}
+	
+	@Test
+	@DisplayName("This is a test with @DisplayName annotation")
+	fun displayNameExample() {
 		assertEquals(123, 123)
 	}
 
